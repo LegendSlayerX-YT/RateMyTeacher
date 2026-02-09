a7481b81aa04942d74bb5946a7ad3dceeda31e82
Henry Chen <chenhenrybunny@gmail.com>
2026-02-09 09:47:14 -0800

    comment

--- backend/play.py
+++ backend/play.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_world() -> str:
     return "Hello, World!"
+
+
+"""
+Example:
+http://127.0.0.1:5000/teacher/aaa?id=bbb&key=value
+
+function argument id will be 'aaa'
+request.args will be a map {'id': 'bbb', 'key': 'value'}
+
+request.data can only be populated when using POST method.
+The advantage of POST method is that the body is not subjust to URL length limitation 
+"""
+@app.route("/teacher/<id>", methods=['GET', 'POST'])
+def query_teacher(id: str) -> str:
+    return f"""
+    You are quering teacher: <br>
+    using method {request.method} <br>
+    function argument {id} <br>
+    request.args {request.args.get("id")} <br>
+    request.data {request.data} <br>
+"""
 
 if __name__ == "__main__":
     app.run()
